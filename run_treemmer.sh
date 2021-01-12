0fafb429aa59f210210c66d26e1bee38a25857de
Alexander Lucaci <43097993+aglucaci@users.noreply.github.com>
2021-01-12 16:36:27 -0500

    Update run_treemmer.sh
    showing setup

--- run_treemmer.sh
+++ run_treemmer.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#git clone https://git.scicore.unibas.ch/TBRU/Treemmer.git
+#mkdir -p testing
+#cd testing
+
+#1
+#python ../Treemmer_v0.3.py testing/MACSE2_Out_Codons_recombinants_0.fas.treefile
+
+#2
 python ../Treemmer_v0.3.py testing/MACSE2_Out_Codons_recombinants_0.fas.treefile --verbose 1 -RTL 0.8
 
 # end of file
